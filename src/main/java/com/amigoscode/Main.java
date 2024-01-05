52f077284b02d824915da1e619f6812b9c7682f8
Marvin Céspedes <mcespo@gmail.com>
2024-01-04 18:09:18 -0600

    record Jackson sugar

--- src/main/java/com/amigoscode/Main.java
+++ src/main/java/com/amigoscode/Main.java
@@ -13,7 +13,9 @@ public class Main {
     }
 
     @GetMapping("/")
-    public String greet() {
-        return "hey there!";
+    public GreetResponse greet() {
+        return new GreetResponse("hello child!");
     }
+
+    record GreetResponse(String greet){}
 }
